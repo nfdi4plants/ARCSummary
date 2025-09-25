1d9a840daa5dc03eff383697a66a2c3cb3964d5e
olscholz <ole.scho202@gmail.com>
2025-09-25 17:00:37 +0200

    Added tests Folder structure for future unit tests

---
+++ tests/ARCSummary.Tests/SummaryTests.fs
@@ -0,0 +1,21 @@
+﻿module ARCSummary.Tests
+
+open ARCtrl
+open Expecto
+open System
+open System.IO
+
+
+
+
+
+let failTest = 
+    test "AlwaysFail" {
+        let actual = true
+        Expect.isFalse actual "Expected value to be false, but was true instead"
+    }
+
+[<EntryPoint>]
+let main argv =
+  runTestsWithCLIArgs [] argv failTest
+
